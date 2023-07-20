b68478a98095633620917641e325187b8ac93d5a
Sibylse <45455846+Sibylse@users.noreply.github.com>
2023-07-20 23:50:49 +0200

    Update loss.py

--- train/loss.py
+++ train/loss.py
@@ -45,6 +45,33 @@ class CE_CTLoss(nn.Module):
         torch.clamp_(self.delta, self.delta_min, self.delta_max)
         self.classifier.prox()
 
+class CTLoss(nn.Module):
+    def __init__(self, classifier, c, device, delta_min = 0.001, delta_max = 0.999):
+        super(CE_CTLoss, self).__init__()
+        self.I = torch.eye(c).to(device)
+        self.ce_loss = nn.CrossEntropyLoss()
+        self.nll_loss = nn.NLLLoss()
+        self.classifier = classifier.to(device)
+        self.delta = nn.Parameter(torch.ones(c)*0.9)
+        self.delta_min = delta_min
+        self.delta_max = delta_max
+ 
+    def forward(self, inputs, targets):        
+        Y = self.I[targets].float().unsqueeze(1) #m x c
+        logits_views = self.classifier(inputs) # m x d/d_view x c
+        logits_views = Y*logits_views + self.delta*(1-Y)*logits_views
+        logits = logits_views.transpose(1,2)
+        loss = self.ce_loss(logits,targets) 
+        loss+= self.nll_loss(logits,targets)
+        return loss
+    
+    def conf(self,inputs):
+        return self.classifier.conf(inputs)
+    
+    def prox(self):
+        torch.clamp_(self.delta, self.delta_min, self.delta_max)
+        self.classifier.prox()
+
 
 class BCE_DUQLoss(nn.Module):
     
