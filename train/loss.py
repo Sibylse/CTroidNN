ff5dde6e99b1ba613f56c8973a12592383684ee3
Sibylse <45455846+Sibylse@users.noreply.github.com>
2023-08-09 11:16:32 +0200

    Update loss.py

--- train/loss.py
+++ train/loss.py
@@ -17,17 +17,17 @@ def gradient_penalty(self, inputs, outputs):
     gradient_penalty = ((grad_norm - 1) ** 2).mean()
     return gradient_penalty
     
-class CE_Loss:
+class CE_Loss(nn.Module):
     def __init__(self, c, device):
-        #super(CE_Loss, self).__init__()
+        super(CE_Loss, self).__init__()
         self.ce_loss = nn.CrossEntropyLoss()
         #self.classifier = classifier.to(device)
         self.softmax = nn.Softmax(dim=1)
-        self.logits = 0
+        #self.logits = 0
  
-    #def forward(self, inputs, targets):  
-    #    self.logits = self.classifier(inputs) # prediction before softmax
-    #    return self.ce_loss(self.logits, targets)
+    def forward(self, logits, targets):  
+        #self.logits = self.classifier(inputs) # prediction before softmax
+        return self.ce_loss(logits, targets)
 
     def loss(self, inputs, targets, net):
         logits = net(inputs)
@@ -40,31 +40,31 @@ class CE_Loss:
     def prox(self, net):
         return
 
-class CTLoss():
+class CTLoss(nn.Module):
     def __init__(self, c, device):
-        #super(CTLoss, self).__init__()
+        super(CTLoss, self).__init__()
         #self.I = torch.eye(c).to(device)
         self.ce_loss = nn.CrossEntropyLoss()
         self.nll_loss = nn.NLLLoss()
         #self.classifier = classifier.to(device)
  
-    #def forward(self, inputs, targets):        
+    def forward(self, logits_views, targets):        
         #Y = self.I[targets].float().unsqueeze(1) #m x c
     #    logits_views = self.classifier(inputs) # m x d/d_view x c
         #logits_views = Y*logits_views + self.delta*(1-Y)*logits_views
-    #    logits = logits_views.transpose(1,2)
-    #    targets_rep = targets.repeat(logits.size(2),1).t()
-    #    loss = self.ce_loss(logits,targets_rep) 
-    #    loss+= self.nll_loss(logits,targets_rep)
-    #    return loss
-
-    def loss(self, inputs, targets, net):
-        logits_views = net(inputs) # m x d/d_view x c
         logits = logits_views.transpose(1,2)
         targets_rep = targets.repeat(logits.size(2),1).t()
         loss = self.ce_loss(logits,targets_rep) 
         loss+= self.nll_loss(logits,targets_rep)
-        return loss, torch.exp(torch.sum(logits_views,1))
+        return loss
+
+    def loss(self, inputs, targets, net):
+        logits_views = net(inputs) # m x d/d_view x c
+        #logits = logits_views.transpose(1,2)
+        #targets_rep = targets.repeat(logits.size(2),1).t()
+        #loss = self.ce_loss(logits,targets_rep) 
+        #loss+= self.nll_loss(logits,targets_rep)
+        return self.forward(logits_views, targets), torch.exp(torch.sum(logits_views,1))
     
     def conf(self, inputs, net):
         logits_views = net(inputs)
@@ -75,10 +75,10 @@ class CTLoss():
         net.classifier.prox()
 
 
-class BCE_DUQLoss():
+class BCE_DUQLoss(nn.Module):
     
     def __init__(self, c, device, weight_gp):
-        #super(BCE_DUQLoss, self).__init__()
+        super(BCE_DUQLoss, self).__init__()
         #self.bce_loss = nn.BCELoss()
         self.I = torch.eye(c).to(device)
         self.weight_gp = weight_gp
@@ -87,28 +87,29 @@ class BCE_DUQLoss():
         #self.Y_pred = 0 #predicted class confidences
         self.Y= 0
     
-    #def forward(self, inputs, targets):
-    #    self.Y = self.I[targets].float()
-    #    self.Y_pred = torch.exp(self.classifier(inputs))
-    #    #loss = self.bce_loss(self.Y_pred, self.Y)
-    #    loss = F.binary_cross_entropy(self.Y_pred, self.Y, reduction="mean")
-    #    return loss
+    def forward(self, logs, targets):
+        self.Y = self.I[targets].float()
+        Y_pred = torch.exp(logs)
+        #loss = self.bce_loss(Y_pred, self.Y)
+        loss = F.binary_cross_entropy(Y_pred, self.Y, reduction="mean")
+        return loss
 
     def loss(self, inputs, targets, net):
         if self.weight_gp >0:
             inputs.requires_grad_(True)
         self.Y = self.I[targets].float()
         self.embedding = net.embed(inputs)
-        Y_pred = torch.exp(net.classifier(self.embedding))
-        loss = F.binary_cross_entropy(Y_pred, Y, reduction="mean")
+        logs = net.classifier(self.embedding)
+        Y_pred = torch.exp(logs)
+        loss = F.binary_cross_entropy(Y_pred, self.Y, reduction="mean")
         if self.weight_gp > 0:
             gp = gradient_penalty(inputs, Y_pred)
-            loss += weight_gp_pred * gp
+            loss += self.weight_gp * gp
         return loss, Y_pred
     
     def conf(self, inputs, net):
-        logits = net(inputs)
-        return torch.exp(logits)
+        logs = net(inputs)
+        return torch.exp(logs)
     
     def prox(self, net):
         net.eval()
