82843c7c348322737d5336b382b8f68c712e4cc3
Sibylse <45455846+Sibylse@users.noreply.github.com>
2023-08-22 16:52:13 +0200

    Update lenet.py

--- train/models/lenet.py
+++ train/models/lenet.py
@@ -43,7 +43,7 @@ class LeNetEmbed(nn.Module):
 #        return out     
     
 class LeNet(nn.Module):
-    def __init__(self,embedding_dim, classifier,coeff=0,n_power_iterations=1):
+    def __init__(self,embedding_dim, classifier,coeff=None,n_power_iterations=1):
         super(LeNet, self).__init__()
         self.embed = LeNetEmbed(embedding_dim=embedding_dim, coeff=coeff, n_power_iterations=n_power_iterations)
         self.classifier = classifier
