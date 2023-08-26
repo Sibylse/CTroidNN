8c85c944da23e0b3ce021d5c419d564a00cdd21f
Sibylse <45455846+Sibylse@users.noreply.github.com>
2023-08-26 17:10:04 +0200

    Update layers.py

--- train/layers.py
+++ train/layers.py
@@ -38,9 +38,9 @@ class CTroidDO(nn.Module):
         if bias:
             self.bias = nn.Parameter(torch.empty(out_features))
             if self.bias is not None:
-            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.squared_distances.weight)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            nn.init.uniform_(self.bias, -bound, bound)
+                fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.squared_distances.weight)
+                bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+                nn.init.uniform_(self.bias, -bound, bound)
         else:
             self.register_parameter('bias', None)
         self.gamma_min = gamma_min
